ad91a21cacbd53c3b2238f58930e72c3642cb1f4
Terrell Cooper <terrellcoop@gmail.com>
2020-07-24 00:45:33 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,11 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+  passengers.each do |room, name| 
+    if room == :suite_a && name.start_with?("A") 
+    winner = name 
+   end 
+ end 
+winner
 end
